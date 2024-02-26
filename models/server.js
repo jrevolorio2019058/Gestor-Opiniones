78209ddaaacbe091e363d3c97372d8645978211d
jrevolorio2019058 <jrevolorio-2019058@kinal.edu.gt>
2024-02-26 00:16:46 -0600

    Creando conexion con servidor | Puerto 3000

---
+++ models/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+
+const cors = require('cors');
+
+class Server{
+
+    constructor(){
+
+        this.app = express();
+        this.port = process.env.PORT;
+
+        this.middlewares();
+
+    }
+
+    middlewares(){
+
+        this.app.use(express.static('public'));
+        this.app.use(cors());
+        this.app.use(express.json());
+
+    }
+
+    listen(){
+
+        this.app.listen(this.port, () =>{
+
+            console.log('SERVER ONLINE | ON PORT: ', this.port);
+
+        });
+
+    }
+
+}
+
+module.exports = Server;
